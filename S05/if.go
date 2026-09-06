ad2262b33c1c1243ca61cda0a663824e48245c8d
Mehdi Almasifar <gunflixhunter@gmail.com>
2026-09-06 20:36:04 +0330

    test

--- S05/if.go
+++ S05/if.go
@@ -52,49 +52,82 @@ func main() {
 	// 	println("😢")
 	// }
 	// ********** fallthrough *********
-	var month int
-	println("please enter your month")
-	fmt.Scanln(&month)
-//
-	var totaldays int = 0
+	//var month int
+	//println("please enter your month")
+	//fmt.Scanln(&month)
+	//
+	//var totaldays int = 0
+	//
+	//switch month {
+	//case 12:
+	//	totaldays += 29
+	//	fallthrough
+	//case 11:
+	//	totaldays += 30
+	//	fallthrough
+	//case 10:
+	//	totaldays += 30
+	//	fallthrough
+	//case 9:
+	//	totaldays += 30
+	//	fallthrough
+	//case 8:
+	//	totaldays += 30
+	//	fallthrough
+	//case 7:
+	//	totaldays += 30
+	//	fallthrough
+	//case 6:
+	//	totaldays += 31
+	//	fallthrough
+	//case 5:
+	//	totaldays += 31
+	//	fallthrough
+	//case 4:
+	//	totaldays += 31
+	//	fallthrough
+	//case 3:
+	//	totaldays += 31
+	//	fallthrough
+	//case 2:
+	//	totaldays += 31
+	//	fallthrough
+	//case 1:
+	//	totaldays += 31
+	//
+	//}
+	//println(totaldays)
+	var mounth int
 
-	switch month {
-	case 12:
-		totaldays += 29
-		fallthrough
-	case 11:
-		totaldays += 30
-		fallthrough
-	case 10:
-		totaldays += 30
-		fallthrough
-	case 9:
-		totaldays += 30
-		fallthrough
-	case 8:
-		totaldays += 30
-		fallthrough
-	case 7:
-		totaldays += 30
-		fallthrough
-	case 6:
-		totaldays += 31
-		fallthrough
-	case 5:
-		totaldays += 31
-		fallthrough
-	case 4:
-		totaldays += 31
-		fallthrough
-	case 3:
-		totaldays += 31
-		fallthrough
-	case 2:
-		totaldays += 31
-		fallthrough
-	case 1:
-		totaldays += 31
+	fmt.Println("please enter your mounth in number")
+	fmt.Scanln(&mounth)
 
+	if mounth == 1 {
+		fmt.Println("farvardin")
+
+	} else if mounth == 2 {
+		fmt.Println("ordibesht")
+	} else if mounth == 3 {
+		fmt.Println("khordad")
+	} else if mounth == 4 {
+		fmt.Println("tir")
+	} else if mounth == 5 {
+		fmt.Println("mordad")
+	} else if mounth == 6 {
+		fmt.Println("sharivar")
+	} else if mounth == 7 {
+		fmt.Println("mehr")
+	} else if mounth == 8 {
+		fmt.Println("aban")
+	} else if mounth == 9 {
+		fmt.Println("azar")
+	} else if mounth == 10 {
+		fmt.Println("dey")
+	} else if mounth == 11 {
+		fmt.Println("bahman")
+	} else if mounth == 12 {
+		fmt.Println("esfand")
+	} else {
+		print("koskesh")
 	}
-	println(totaldays)
 }
